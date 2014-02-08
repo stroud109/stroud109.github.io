2fe9b55658f482bcb637ee4f619e5f24a07a7139
Stephanie Stroud <stephanie.marie.stroud@gmail.com>
2014-02-07 22:55:54 -0800

    persisted todos to local storage

--- todos/javascript.js
+++ todos/javascript.js
@@ -1,28 +1,56 @@
-// TODO:
-// remember the state of the todo list, so upon refresh it's still there
-// listen to submit event on form rather than keypress event
-
 var $button = $('#create');
 var $input = $('#input');
 var $list = $('#list');
 var $checkmark = $('#checkmark');
+var persist = function() {
+    var currentToDos = [];
+    $list.find('.panel:visible').each(function(){
+        var todo = $(this);
+        currentToDos.push({
+            text: todo.text(),
+            done: todo.hasClass('done'),
+        });
+    });
+    localStorage.todoList = JSON.stringify(currentToDos);
+};
 
-$input.keypress(function(e) {
-    if(e.which == 13) {
-        $list.append('<div class="panel panel-default panel-body">' +
-            $input.val() +
-            '<span class="navbar-right todo x-mark"><span class="glyphicon glyphicon-remove" data-action="delete"></span></span>' +
-            '<span class="navbar-right todo checkmark"><span class="glyphicon glyphicon-ok" data-action="done"></span></span></div>');
-        $input.val('');
+var loadFromStorage = function() {
+    var todoList = localStorage.todoList;
+    if(!todoList){
+        return;
     }
+    var todos = JSON.parse(todoList);
+    for(var i = 0; i < todos.length; i++){
+        $list.append(todoTemplate(todos[i].text, todos[i].done));
+    }
+};
+
+var todoTemplate = function(text, isDone){
+    return '<div class="panel panel-default panel-body ' +
+            (isDone ? 'done':'') +'">' + text +
+            '<span class="navbar-right todo x-mark"><span class="glyphicon glyphicon-remove" data-action="delete"></span></span>' +
+            '<span class="navbar-right todo checkmark"><span class="glyphicon glyphicon-ok" data-action="done"></span></span></div>';
+};
+
+$('.input-form').on('submit', function(e) {
+    e.preventDefault();
+    $list.append(todoTemplate($input.val(), false));
+    $input.val('');
+    persist();
 });
 
 $list.on('click', 'div [data-action=done]', function (e){
     var panel = $(e.target).closest('.panel');
     panel.addClass('done');
     $list.append(panel);
+    persist();
 });
 
 $list.on('click', 'div [data-action=delete]', function (e){
-    $(e.target).closest('.panel').fadeOut();
+    $(e.target).closest('.panel').fadeOut(persist);
+});
+
+
+$(function(){
+    loadFromStorage();
 });
